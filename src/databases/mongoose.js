dbb4890cb4582e60efb5d1796e52824cd4af5af6
ameer hamza <mianameer998@gmail.com>
2022-12-26 10:44:50 +0500

    .

--- src/databases/mongoose.js
+++ src/databases/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://127.0.0.1:27017/backend-template", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
