97f103283cd36a44650684f8d1c73ec0c5007310
TeamDlxx <52232678+TeamDlxx@users.noreply.github.com>
2023-02-21 14:55:42 +0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+    const res = await axios.get('https://geolocation-db.com/json/')
+    console.log(res.data, 'Server Ip')
+    // setIP(res.data.IPv4)
   res.render("index", {title: "Backend Template | API Services"});
 });
 
