ffc87d3a418744ae912bfbe2ecdcf7b913fdab53
muneeb islam <muneebch6@gmail.com>
2024-02-06 11:38:58 +0500

    testing flow11

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", {title: "Backend Template testing| API Services"});
+  res.render("index", {title: "Backend Template testing1122| API Services"});
 });
 
 module.exports = router;
