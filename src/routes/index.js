0ad9cc8845faba66f8ad07b74f233caffcba10a5
muneeb islam <muneebch6@gmail.com>
2024-02-12 17:12:25 +0500

    testing pipeline

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", {title: "Backend Template testing runnning | API Services"});
+  res.render("index", {title: "Backend Template testing runnning11 | API Services"});
 });
 
 module.exports = router;
